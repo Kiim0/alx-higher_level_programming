f7b9c92fe330665df3793e065c8621862a56ce41
Kiim0 <kareemeldin123@gmail.com>
2023-12-04 01:59:34 -0800

    test

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 1-element_at.py
+# Kiim0
+
+def element_at(my_list, idx):
+    """Retrive an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
